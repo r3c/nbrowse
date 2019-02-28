36809acda698cad49c8601439a8ae907dee1d39e
Remi Caput <git@mirari.fr>
2019-02-28 23:46:17 +0100

    Create one archive per runtime instead of a global one.
    Also remove centos version from default package script.

--- package.sh
+++ package.sh
@@ -1,32 +1,32 @@
 #!/bin/sh -e
 
-current="$(dirname "$0")"
+base="$(dirname "$0")"
 root=nbrowse
-source="$(mktemp -d)"
 
 # Retreive latest version from current HEAD
-version="$(git --work-tree "$current" tag --points-at HEAD)"
+version="$(git --work-tree "$base" tag --points-at HEAD)"
 
 if [ -z "$version" ]; then
 	echo >&2 "error: current HEAD doesn't point to a tag"
 	exit 1
 fi
 
-# Prepare archive directory structure
-mkdir "$source/$root"
+# Create archive for each target runtime
+framework=netcoreapp2.1
+source="$(mktemp -d)"
 
-for runtime in centos-x64 debian-x64 osx-x64 ubuntu-x64 win-x64; do
-	dotnet publish -c Release -r "$runtime" -v quiet "$current/NBrowse.CLI"
+for runtime in debian-x64 osx-x64 ubuntu-x64 win-x64; do
+	dotnet publish -c Release -r "$runtime" -v quiet "$base/NBrowse.CLI"
 
-	ln -s "$(realpath "$current/NBrowse.CLI/bin/Release/netcoreapp2.1/$runtime/publish")" "$source/$root/$runtime"
+	ln -s "$(realpath "$base/NBrowse.CLI/bin/Release/$framework/$runtime/publish")" "$source/$root"
+
+	archive="$root-v$version-$runtime.zip"
+
+	( cd "$source" && zip -qr "$archive" "$root" )
+
+	mv "$source/$archive" "$base/$archive"
+	rm "$source/$root"
 done
-
-# Create archive file
-archive="$root-v$version.zip"
-
-( cd "$source" && zip -qr "$archive" "$root" )
-
-mv "$source/$archive" "$current/$archive"
 
 # Cleanup
 rm -r "$source"
