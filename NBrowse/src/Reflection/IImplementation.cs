3e04051aa0fdb15b24981020f922552cdcca9f6d
Remi Caput <git@mirari.fr>
2020-01-24 20:51:13 +0100

    Add missing description to `IImplementation` properties.

--- NBrowse/src/Reflection/IImplementation.cs
+++ NBrowse/src/Reflection/IImplementation.cs
@@ -1,11 +1,17 @@
 using System.Collections.Generic;
+using System.ComponentModel;
+using Newtonsoft.Json;
 
 namespace NBrowse.Reflection
 {
 	public interface IImplementation
 	{
+		[Description("Methods referenced in code")]
+		[JsonIgnore]
 		IEnumerable<IMethod> ReferencedMethods { get; }
 
+		[Description("Types referenced in code")]
+		[JsonIgnore]
 		IEnumerable<IType> ReferencedTypes { get; }
 	}
 }
